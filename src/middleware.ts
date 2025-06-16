7aaf6e44cb8afa97d5f6f5c864dc4529698b5a80
Zach Bresler <zbresler16@gmail.com>
2025-06-16 20:37:17 +0200

    chore: remove unused var

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
