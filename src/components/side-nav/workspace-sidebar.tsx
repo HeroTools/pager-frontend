1274d6e0cf11993f22869eb0bef0b42137863158
Gabriel Stein <gabrielstein416@gmail.com>
2025-08-11 08:53:00 -0700

    style: fixed workspace sidebar border colour

--- src/components/side-nav/workspace-sidebar.tsx
+++ src/components/side-nav/workspace-sidebar.tsx
@@ -68,7 +68,7 @@ export const WorkspaceSidebar = () => {
   }
 
   return (
-    <div className="flex flex-col h-full ">
+    <div className="flex flex-col h-full">
       <WorkspaceHeader
         workspace={getWorkspace.data}
         isAdmin={getWorkspace.data.user_role === 'admin'}
